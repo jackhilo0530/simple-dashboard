ab5b2b1381a00a2364062ee5803b098665f2b351
mknabb92-blip <mknabb92@gmail.com>
2026-01-26 22:02:42 -0600

    fix:header's css style match

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -5,11 +5,8 @@ const Header: React.FC = () => {
 
     return (
         <header className='sticky top-0 flex w-full bg-white border-gray-200 z-99999 border-b'>
-            <div className='flex flex-row items-center justify-between grow px-6'>
-                <div className='flex items-center justify-end w-full gap-4 px-0 py-4 shadow-none'>
-
-                    <LogoutButton />
-                </div>
+            <div className='flex items-center justify-end w-full gap-4 px-0 py-3 shadow-none'>
+                <LogoutButton />
             </div>
         </header>
     )
